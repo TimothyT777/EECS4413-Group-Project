a466932333684892254a00caf6ad3c6646ef8193
Timdtoll <ttolstinev@gmail.com>
2026-03-03 21:45:37 -0500

    Starter project into main

---
+++ placeholderName/src/main/java/com/example/placeholdername/Controller/testController.java
@@ -0,0 +1,13 @@
+package com.example.placeholdername.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class testController {
+
+    @GetMapping("/api/test")
+    public String test() {
+        return "Hello, World!";
+    }
+}
