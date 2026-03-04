97714f65db9cfd6b2796eed0746ffa4e3d171b70
Timdtoll <ttolstinev@gmail.com>
2026-03-03 22:06:29 -0500

    actual fix hopefully

---
+++ placeholderName/frontend/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+import React, { useState, useEffect } from 'react';
+
+function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch('api/test').then(response => response.text()).then(data => setMessage(data));
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>{message}</h1>
+    </div>
+  )
+}
+
+export default App;
