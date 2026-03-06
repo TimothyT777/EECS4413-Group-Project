4b460128e5ee570a88531efe612839f57af2e77f
eric2500-yorku <eric2500@my.yorku.ca>
2026-03-06 00:44:38 -0500

    Added css and js for index, removed message in App.js

--- placeholderName/frontend/src/index.js
+++ placeholderName/frontend/src/index.js
@@ -11,6 +11,29 @@ root.render(
   </React.StrictMode>
 );
 
+document.addEventListener('DOMContentLoaded', () => {
+  window.addEventListener('scroll', function() {
+    // Measures the current scroll position on the page.
+    const scrollPosition = window.scrollY;
+
+    // Fades the initial greeting background image out while scrolling down.
+    const background = document.querySelector('.background');
+    let newOpacity = 1 - scrollPosition / 500;
+    if (newOpacity < 0) newOpacity = 0;
+    background.style.opacity = newOpacity;
+
+    const scrollText = document.querySelector('.text-slide');
+    const scrollCatalogue = document.querySelector('.catalogue');
+    if (scrollPosition > 400) {
+      scrollText.classList.add('active')
+      scrollCatalogue.classList.add('visible')
+    }
+    else {
+      scrollText.classList.remove('active');
+      scrollCatalogue.classList.remove('visible');
+    }
+  });
+});
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
