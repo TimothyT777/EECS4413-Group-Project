8165ae51be7355d16627affa9db53de0548b00ee
Timdtoll <ttolstinev@gmail.com>
2026-03-12 16:41:38 -0400

    Created database and classes for products, user, admin, and customer as well as required repository and service classes

--- placeholderName/src/main/java/com/example/placeholdername/Controller/testController.java
+++ placeholderName/src/main/java/com/example/placeholdername/Controller/testController.java
@@ -1,4 +1,4 @@
-package com.example.placeholdername.Controller;
+package com.example.placeholdername.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
---
+++ placeholderName/src/main/java/com/example/placeholdername/service/ProductService.java
@@ -0,0 +1,44 @@
+package com.example.placeholdername.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.placeholdername.model.Product;
+import com.example.placeholdername.repository.ProductRepository;
+
+@Service
+public class ProductService { 
+
+    private final ProductRepository productRepo;
+
+    public ProductService(ProductRepository productRepo) {
+        this.productRepo = productRepo;
+    }
+
+    public List<Product> getAllProducts() {
+        return productRepo.findAll();
+    }
+
+    public List<Product> findByName(String name) {
+        return productRepo.findByName(name);
+    }
+
+    public void clear() {
+        productRepo.deleteAll();
+    }
+
+    public void removeProductById(Long id) {
+        productRepo.deleteById(id);
+    }
+
+    public boolean productExists(Long id) {
+        return productRepo.existsById(id);
+    }
+
+    /**
+     * For the person doing the product catalogue filtering, you are going 
+     * to want to do it here and write a function to return a list of products.
+     * that match the filter criteria from either a search query or something else.
+     */
+}
