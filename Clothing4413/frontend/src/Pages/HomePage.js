fd49052c1ec57e7a089291d2bd12fc2adc38b050
eric2500-yorku <eric2500@my.yorku.ca>
2026-03-21 23:01:28 -0400

    Added filtering to catalogue
    Can now filter objects in the catalogue by search term, price range, category, and brand. Can also sort by alphabetical or price.

--- Clothing4413/frontend/src/Pages/HomePage.js
+++ Clothing4413/frontend/src/Pages/HomePage.js
@@ -4,22 +4,49 @@ import '../index.css';
 
 function HomePage(){
 	const [searchTerm, setSearchTerm] = useState("");
-	const [selectedItem, setSelectedItem] = useState(null);
+	const [selectedProduct, setSelectedProduct] = useState(null);
+	const [selectedBrand, setSelectedBrand] = useState("");
+	const [selectedCategory, setSelectedCategory] = useState("");
+	const [sortOrder, setSortOrder] = useState("");
+	const [minPrice, setMinPrice] = useState("");
+	const [maxPrice, setMaxPrice] = useState("");
+
 
 	// Used for searching up items and pulling info from the database.
-	const items = [
-			{ name: "Shirt 1", price: 19.99, image: "/img/shirt1.png", description: "Description for shirt ONE here" },
-			{ name: "Shirt 2", price: 23.99, image: "/img/shirt2.png", description: "Description for shirt TWO here" },
-			{ name: "Shirt 3", price: 27.99, image: "/img/shirt3.png", description: "Description for shirt THREE here" },
-			{ name: "Shirt 4", price: 15.99, image: "/img/shirt4.png", description: "The FitnessGram™ Pacer Test is a multistage aerobic capacity test that progressively gets more difficult as it continues. The 20 meter pacer test will begin in 30 seconds. Line up at the start. The running speed starts slowly, but gets faster each minute after you hear this signal. [beep] A single lap should be completed each time you hear this sound. [ding] Remember to run in a straight line, and run as long as possible. The second time you fail to complete a lap before the sound, your test is over. The test will begin on the word start. On your mark, get ready, start." },
+	const products = [
+			{ name: "Shirt 1", price: 19.99, stock: 5, image: "/img/shirt1.png", category: "Shirt", brand: "Gucci"	, description: "Description for shirt ONE here" },
+			{ name: "Shirt 2", price: 23.99, stock: 3, image: "/img/shirt2.png", category: "Shirt", brand: "Gucci"	, description: "Description for shirt TWO here" },
+			{ name: "Shirt 3", price: 27.99, stock: 1, image: "/img/shirt3.png", category: "Shirt", brand: "Nike"	, description: "Description for shirt THREE here" },
+			{ name: "Shirt 4", price: 15.99, stock: 99, image: "/img/shirt4.png", category: "Pants", brand: "Your Mom", description: "The FitnessGram™ Pacer Test is a multistage aerobic capacity test that progressively gets more difficult as it continues. The 20 meter pacer test will begin in 30 seconds. Line up at the start. The running speed starts slowly, but gets faster each minute after you hear this signal. [beep] A single lap should be completed each time you hear this sound. [ding] Remember to run in a straight line, and run as long as possible. The second time you fail to complete a lap before the sound, your test is over. The test will begin on the word start. On your mark, get ready, start." },
 		];
-	const searchedItems = items.filter(
-		(item) => item.name.toLowerCase().includes(searchTerm.toLowerCase())
-	);
+
+	// Advanced filters combing search term, category, brand, and price range.
+	const brands = [...new Set(products.map(product => product.brand))];
+	const category = [...new Set(products.map(product => product.category))];
+	const filteredProduct = products.filter((products) => {
+		const matchedTerm = products.name.toLowerCase().includes(searchTerm.toLowerCase())
+		const matchedCategory = selectedCategory ? products.category === selectedCategory : true;
+		const matchedBrand = selectedBrand ? products.brand === selectedBrand : true;
+		const matchedMinPrice = minPrice !== "" ? products.price >= parseFloat(minPrice) : true;
+		const matchedMaxPrice = maxPrice !== "" ? products.price <= parseFloat(maxPrice) : true;
+		return matchedTerm && matchedCategory && matchedBrand && matchedMinPrice && matchedMaxPrice;
+	});
+	
+	// Sort by alphabetical or by price.
+	if (sortOrder === "asc") {
+		filteredProduct.sort((a, b) => a.name.localeCompare(b.name));
+	} else if (sortOrder === "desc") {
+		filteredProduct.sort((a, b) => b.name.localeCompare(a.name));
+	} else if (sortOrder === "price-asc") {
+		filteredProduct.sort((a, b) => a.price - b.price);
+	} else if (sortOrder === "price-desc") {
+		filteredProduct.sort((a, b) => b.price - a.price);
+	}
+	
 
 	// Handles clicking on catalogue items.
-	const handleItemClick = (item) => setSelectedItem(item);
-	const handleClose = () => setSelectedItem(null);
+	const handleItemClick = (products) => setSelectedProduct(products);
+	const handleClose = () => setSelectedProduct(null);
 
 	useEffect(() => {
 		// Captures the page scroll and causes effects accordingly.
@@ -37,20 +64,23 @@ function HomePage(){
 			const scrollText = document.querySelector('.text-slide');
 			const scrollCatalogue = document.querySelector('.catalogue');
 			const searchCatalogue = document.querySelector('.search-bar');
+			const searchAdvanced = document.querySelector('.search-advanced');
 			if (scrollPosition > 400) {
 				scrollText.classList.add('active');
 				scrollCatalogue.classList.add('visible');
 				searchCatalogue.classList.add('visible');
+				searchAdvanced.classList.add('visible');
 			}
 			else {
 				scrollText.classList.remove('active');
 				scrollCatalogue.classList.remove('visible');
 				searchCatalogue.classList.remove('visible');
+				searchAdvanced.classList.remove('visible');
 			}
 		}
 		window.addEventListener('scroll', handleScroll);
 		return () => window.removeEventListener('scroll', handleScroll);
-	}, []);
+	});
 
 	
 	
@@ -65,8 +95,11 @@ function HomePage(){
 				<h3>You can tell by the way it is</h3>
 			</div>
 
+			{/*Everything catalogue-related begins here */}
 			<div className="text-slide">Our (Legitimate) Catalogue</div>
 			<div className="catalogue-wrapper">
+
+				{/*Search functions via keywords*/}
 				<input 
 					type='text' 
 					placeholder='Search Items...' 
@@ -74,29 +107,70 @@ function HomePage(){
 					onChange={(e) => setSearchTerm(e.target.value)}
 					className='search-bar'
 				/>
+
+				{/*Advanced filters for sorting, categories, and brands*/}
+				<div className="search-advanced">
+					<div><label>Sort By:</label>
+						<select value={sortOrder} onChange={(e) => setSortOrder(e.target.value)}>
+							<option value="">Default</option>
+							<option value="asc">A - Z</option>
+							<option value="desc">Z - A</option>
+							<option value="price-asc">Price: Low - High</option>
+							<option value="price-desc">Price: High - Low</option>
+						</select>
+					</div>
+					<div><label>Price Range:</label>
+						<div className="price-range">
+							<input type="number" placeholder="Min" value={minPrice} onChange={(e) => setMinPrice(e.target.value)} min="0"/>
+							<input type="number" placeholder="Max" value={maxPrice} onChange={(e) => setMaxPrice(e.target.value)} min="0"/>
+						</div>
+					</div>
+					<div><label>Category:</label>
+						<select value={selectedCategory} onChange={(e) => setSelectedCategory(e.target.value)}>
+							<option value="">Any</option>
+							{category.map((category) => (
+								<option key={category} value={category}>{category}</option>
+							))}
+						</select>
+					</div>
+					<div><label>Brand:</label>
+						<select value={selectedBrand} onChange={(e) => setSelectedBrand(e.target.value)}>
+							<option value="">Any</option>
+							{brands.map((brand) => (
+								<option key={brand} value={brand}>{brand}</option>
+							))}
+						</select>
+					</div>
+				</div>
+
+				{/*Displays the products as clickable cards*/}
 				<div className="catalogue">
-					{searchedItems.map((item, index) => (
-						<div className='item-card' key={index} onClick={() => handleItemClick(item)}>
-						<img src={item.image} class="image"/>
+					{filteredProduct.map((products, index) => (
+						<div className='product-card' key={index} onClick={() => handleItemClick(products)}>
+						<img src={products.image} class="image"/>
 						<info>
-							<p className="item-name">{item.name}</p>
-							<p className="item-price">${item.price}</p>
+							<p className="product-name">{products.name}</p>
+							<p className="product-price">${products.price}</p>
 						</info>
 						</div>
 					))}
-					{selectedItem && (
-						<div className='item-modal' onClick={handleClose}>
-							<div className='item-content' onClick={(e) => e.stopPropagation()}>
+
+					{/*Displays more info about a product when clicked.
+					   Notably, the modal is separated by left side (product image) and right side (product info)*/}
+					{selectedProduct && (
+						<div className='product-modal' onClick={handleClose}>
+							<div className='product-content' onClick={(e) => e.stopPropagation()}>
 								<span className='close-button' onClick={handleClose}>&times;</span>
-								<div className="item-modal-inner">
-									<div className="item-modal-left">
-										<img src={selectedItem.image} className="item-image-full"/>
+								<div className="product-modal-inner">
+									<div className="product-modal-left">
+										<img src={selectedProduct.image} className="product-image-full"/>
 									</div>
-									<div className="item-modal-right">
-										<div className="item-modal-content">
-											<p className="item-name">{selectedItem.name}</p>
-											<p className="item-price">${selectedItem.price}</p>
-											<p className="item-description">{selectedItem.description}</p>
+									<div className="product-modal-right">
+										<div className="product-modal-content">
+											<p className="product-name">{selectedProduct.name}</p>
+											<p className="product-price">${selectedProduct.price}</p>
+											<p className="product-stock">Only <span className="product-stock-amount">{selectedProduct.stock}</span> in stock!</p>
+											<p className="product-description">{selectedProduct.description}</p>
 										</div>
 										<button className='add-to-cart'>Add to cart</button>
 									</div>
